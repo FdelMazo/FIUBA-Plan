eee72e349bb4e123fa999e2ec70378e9b5b23594
komod0 <juliancrespo15@gmail.com>
2020-09-14 14:10:31 -0300

    Build site

--- docs/precache-manifest.561df33c27bba98b9c6ba3cf3765d880.js
+++ docs/precache-manifest.561df33c27bba98b9c6ba3cf3765d880.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4f1b5573d9574aa0144486dfd25ce5e8",
+    "revision": "d99428478a51206787f79a52cf5601d2",
     "url": "/FIUBA-Plan/index.html"
   },
   {
-    "revision": "be5e91a278b6c67b00db",
+    "revision": "070f8e1a40f4ac4ce50e",
     "url": "/FIUBA-Plan/static/css/2.e428341d.chunk.css"
   },
   {
-    "revision": "f475cf9a49779dfb9366",
+    "revision": "9fc9484895e44d17c32d",
     "url": "/FIUBA-Plan/static/css/main.7619234d.chunk.css"
   },
   {
-    "revision": "be5e91a278b6c67b00db",
-    "url": "/FIUBA-Plan/static/js/2.3f1fe3a4.chunk.js"
+    "revision": "070f8e1a40f4ac4ce50e",
+    "url": "/FIUBA-Plan/static/js/2.7c88a60a.chunk.js"
   },
   {
     "revision": "c6e06eea05547d6ef503a87e42914a93",
-    "url": "/FIUBA-Plan/static/js/2.3f1fe3a4.chunk.js.LICENSE.txt"
+    "url": "/FIUBA-Plan/static/js/2.7c88a60a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f475cf9a49779dfb9366",
-    "url": "/FIUBA-Plan/static/js/main.a9d097f4.chunk.js"
+    "revision": "9fc9484895e44d17c32d",
+    "url": "/FIUBA-Plan/static/js/main.8b424b37.chunk.js"
   },
   {
     "revision": "eb83ed3fcff50053604e",

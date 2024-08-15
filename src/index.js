d42f93635fbb14517dd0c9a5a42c2d06bc81e06f
Federico del Mazo <fdelmazo@fi.uba.ar>
2024-08-15 18:28:46 -0300

    Add a catch-all boundary for unexpected errors

--- src/index.js
+++ src/index.js
@@ -2,17 +2,28 @@ import { ChakraProvider, Flex } from "@chakra-ui/react";
 import "@fontsource/source-sans-pro/400.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ErrorBoundary } from "react-error-boundary";
 import Body from "./components/Body";
-import customTheme from "./theme";
 import { DataProvider } from "./DataContext";
+import customTheme from "./theme";
 
 const App = () => {
   return (
-    <DataProvider>
-      <Flex direction="columns" h="100vh">
-        <Body />
-      </Flex>
-    </DataProvider>
+    <ErrorBoundary
+      fallbackRender={({ error }) => {
+        console.warn(
+          "Hubo un error inesperado. Se limpian los datos guardados",
+          error,
+        );
+        localStorage.setItem("fiubaplan", JSON.stringify({}));
+      }}
+    >
+      <DataProvider>
+        <Flex direction="columns" h="100vh">
+          <Body />
+        </Flex>
+      </DataProvider>
+    </ErrorBoundary>
   );
 };
 

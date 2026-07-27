1eac7c920851674076ed7a266e24593ff3f05610
Federico del Mazo <fdelmazo@fi.uba.ar>
2026-07-27 19:23:07 -0300

    Lint

--- src/siuparser.js
+++ src/siuparser.js
@@ -56,17 +56,17 @@ export function parseSIU(rawdata) {
         codigo: materiaMatch[2],
         cursos: [],
       };
-      console.debug(`- Found materia: ${materia.nombre} (${materia.codigo})`)
+      console.debug(`- Found materia: ${materia.nombre} (${materia.codigo})`);
 
       for (const cursoMatch of materiaFullText.matchAll(cursosPattern)) {
         const cursoCodigo = `${materia.codigo}-${cursoMatch[1]}`;
         const cursoDocentes = cursoMatch[2].trim().replace(/\(.*?\)/g, "");
-        console.debug(`-- Found curso: ${cursoCodigo}`)
+        console.debug(`-- Found curso: ${cursoCodigo}`);
 
         const clases = [];
         for (let claseLine of cursoMatch[3].trim().split("\n")) {
-          console.debug(`--- Found clase: ${claseLine}`)
-          if (!SEMANA.some(day => claseLine.includes(day))) {
+          console.debug(`--- Found clase: ${claseLine}`);
+          if (!SEMANA.some((day) => claseLine.includes(day))) {
             continue;
           }
 
